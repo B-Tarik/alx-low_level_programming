841e0ef47b0fbb78cc855970331d27af11706865
Tarik Bouchekkif <tarik.bouchekkif@gmail.com>
2023-03-17 00:26:56 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,10 @@
  *
  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quote, 59);
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, q, 59);
 	return (1);
 }
