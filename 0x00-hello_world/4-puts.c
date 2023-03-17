b15e04a90a2d7bcb070c625b0aee7b521c6c81b2
B-Tarik <tarik.bouchekkif@gmail.com>
2023-03-17 20:20:00 +0000

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
-#!/bin/bash
-# Simply a wrapper script to keep you from having to use betty-style
-# and betty-doc separately on every item.
-# Originally by Tim Britton (@wintermanc3r), multiargument added by
-# Larry Madeo (@hillmonkey)
+#include <stdio.h>
 
-BIN_PATH="/usr/local/bin"
-BETTY_STYLE="betty-style"
-BETTY_DOC="betty-doc"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-if [ "$#" = "0" ]; then
-    echo "No arguments passed."
-    exit 1
-fi
-
-for argument in "$@" ; do
-    echo -e "\n========== $argument =========="
-    ${BIN_PATH}/${BETTY_STYLE} "$argument"
-    ${BIN_PATH}/${BETTY_DOC} "$argument"
-done
+int main() 
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
+}
