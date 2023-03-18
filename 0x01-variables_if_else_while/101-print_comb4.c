da1c566602db5be95891db456529fa9a2018f351
Tarik Bouchekkif <tarik.bouchekkif@gmail.com>
2023-03-18 17:35:54 +0000

    fix

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * print all possible different combinations of 3 digits.
+ * Return: 0
+ */
+int main(void) {
+    int i, j, k;
+    
+    for(i = 0; i <= 7; i += 1) {
+        for(j = i+1; j <= 8; j += 1) {
+            for(k = j+1; k <= 9; k += 1) {
+                putchar('0'+i);
+                putchar('0'+j);
+                putchar('0'+k);
+                if(!(i == 7 && j == 8 && k == 9)) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return (0);
+}
