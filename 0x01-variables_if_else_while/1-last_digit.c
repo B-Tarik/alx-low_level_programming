ea259816b9488fa4850559f9a6be0022dfaef443
Tarik Bouchekkif <tarik.bouchekkif@gmail.com>
2023-03-18 02:18:47 +0000

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	
+
 	if (m > 5)
 		printf("Last digit of %i is %i and is greater than 5\n",
 		       n, m);
@@ -24,6 +24,6 @@ int main(void)
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 		       n, m);
-	
+
 	return (0);
 }
