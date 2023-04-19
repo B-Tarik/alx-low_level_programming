7f9697e1ad371342aa18007f114cc671d098122e
B-Tarik <tarik.bouchekkif@gmail.com>
2023-04-19 22:06:28 +0000

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
+#include"stdlib.h"
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - Prints a name.
+ * print_name -
  * @name:
- * @f: 
- */
+ * @f:
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
+	if (name != NULL && *name != '\0' && f != NULL)
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,19 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * array_iterator -
  * @array:
  * @size:
  * @action:
- */
+ * Return: nothing
+ **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
+	unsigned int i;
 
-	while (size-- > 0)
+	if (array != NULL && size > 0 && action != NULL)
 	{
-		action(*array);
-		array++;
+		for (i = 0; i < size; i++)
+			action(array[i]);
 	}
 }
