ec1f1be7d0ffdbdd0839420468dd8e7b2c2554d2
B-Tarik <tarik.bouchekkif@gmail.com>
2023-04-28 02:52:17 +0000

    fix

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "lists.h"
+/**
+ * print_list -
+ * @h:
+ * Return:
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "lists.h"
+/**
+ * list_len 
+ * @h: 
+ * Return: 
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
