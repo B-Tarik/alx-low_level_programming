57ab02e2ed888ae743b05377807f237832c5454d
B-Tarik <tarik.bouchekkif@gmail.com>
2023-05-08 16:39:31 +0000

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 	w = write(STDOUT_FILENO, buf, r);
 
-	if (r == -1 || o == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buf);
 		{
